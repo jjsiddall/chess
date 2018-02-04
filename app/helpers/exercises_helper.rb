1bdb3584cde5ebda6dfcc2a2933c6f4791a43a04
Jake <jacobsiddall@gmail.com>
2018-02-04 15:38:05 -0600

    builds Exercise#edit with only pieces that are in use (on the board) or not in use (on the pieces to use)

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,5 +1,5 @@
 module ExercisesHelper
-  def square(rank, file, setup)
+  def board_square(rank, file, setup) #creates a board square and puts a piece in it if setup says it should have one
     setup = eval(setup) if setup.is_a? String
     square_id = (file+96).chr + rank.to_s
     content_tag(:div, "", class: ["square", ('dark' if (rank+file) % 2 == 0)], id: square_id) do
@@ -12,5 +12,21 @@ module ExercisesHelper
         content_tag(:div, piece_html.html_safe, class: "piece #{color}", id: piece_id)
       end
     end
+  end
+
+  # builds a square for a piece to sit in
+  def piece_square(piece_color, piece_id, piece_html, used_pieces)
+    content_tag(:div, "", class: "square") do
+      content_tag(:div, piece_html.html_safe, class: "piece #{piece_color}", id: piece_id) unless used_pieces.include? piece_id
+    end
+  end
+
+  def pieces_on_the_board(setup)
+    setup = eval(setup) if setup.is_a? String
+    pieces = []
+    setup.each do |square|
+      pieces << square.second['id']
+    end
+    return pieces
   end
 end
